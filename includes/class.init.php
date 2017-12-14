d19f75a74afb095f73b607a591a681328728ca1c
HK <hitesh@99fusion.com>
2017-12-14 11:39:48 +0530

    Js for fetching form data added and an ajax call initialised for applying

--- includes/class.init.php
+++ includes/class.init.php
@@ -31,7 +31,7 @@ class WPLMS_Application_Forms_Init{
 
 	function add_wplms_application_forms_in_course( $metabox ){
 		$metabox['vibe_course_apply']['type'] = 'conditionalswitch';
-		$metabox['vibe_course_apply']['hide_nodes'] = array('vibe_wplms_application_forms');
+		$metabox['vibe_course_apply']['hide_nodes'] = array('vibe_wplms_application_forms','vibe_wplms_application_forms_editor');
 		$metabox['vibe_course_apply']['options'] = array('H'=>__('Hide','wplms-af' ),'S'=>__('Show','wplms-af' ));
 
 		$metabox['vibe_wplms_application_forms'] = array(
@@ -70,12 +70,106 @@ class WPLMS_Application_Forms_Init{
 				if( vibe_validate($check_apply_form) ){
 					$check_apply_content = get_post_meta($course_id,'vibe_wplms_application_forms_editor',true);
 					if( !empty($check_apply_content) ){
+						echo '<div class="course_aaplication_form">';
 						echo do_shortcode($check_apply_content);
+						echo '</div>';
+						add_action('wp_footer',array($this,'add_script_for_apply_for_course_button'));
 					}
 				}
 			}
 		}
 		return $course_details;
+	}
+
+	function add_script_for_apply_for_course_button(){
+		?>
+		<script>
+			jQuery(document).ready(function($){
+				$('#apply_course_button').on('click',function(event){
+					var $this = $(this);
+					var form = $('.course_aaplication_form').find('form');
+					if( typeof(form) != 'undefined' ){
+						$this.removeClass('disabled');
+					}else{
+						$this.addClass('disabled');
+					}
+
+					if( $this.hasClass('disabled') ){
+						return;
+					}
+
+					var parent = $('.course_aaplication_form').find('form');
+					var $response= parent.find(".response");
+					var error= '';
+					var data = [];
+					var label = [];
+					var regex = [];
+					regex['email'] = /^([a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$)/i;
+					regex['phone'] = /[A-Z0-9]{7}|[A-Z0-9][A-Z0-9-]{7}/i;
+					regex['numeric'] = /^[0-9]+$/i;
+					regex['captcha'] = /^[0-9]+$/i;
+					regex['required'] = /([^\s])/;
+					var i=0;
+
+					parent.find('.form_field').each(function(){
+						i++;
+						var validate=$(this).attr('data-validate');
+						var value = $(this).val();
+
+						if(!value.match(regex[validate])){
+							error += ' '+vibe_shortcode_strings.invalid_string+$(this).attr('placeholder');
+							$(this).css('border-color','#e16038');
+						}else{
+							data[i]=value;
+							label[i]=$(this).attr('placeholder');
+							if(parent.hasClass('isocharset')){
+								label[i]=encodeURI(label[i]);
+								data[i]=encodeURI(value);
+							}
+
+						}
+						if(validate === 'captcha' && error === ""){
+							var $num = $(this).attr('id');
+							var $sum=$(this).closest('.math-sum');
+							var num1 = parseInt($('#'+$num+'-1').text());
+							var num2 = parseInt($('#'+$num+'-2').text());
+							var sumval = parseInt($(this).val());
+							if( sumval != (num1+num2))
+								error += vibe_shortcode_strings.captcha_mismatch;
+						}
+					});
+
+					var attachment_id = $('.course_aaplication_form').find('.attachment_ids').val();
+
+					if (error !== "") {
+						$response.fadeIn("slow");
+						$response.html("<span style='color:#D03922;'>"+vibe_shortcode_strings.error_string+" " + error + "</span>");
+					}else{
+						var isocharset = false;
+						if(parent.hasClass('isocharset')){
+							isocharset = true;
+						}
+						setTimeout(function(){
+							$.ajax({
+									type: "POST",
+									url: ajaxurl,
+									data: { action: 'submit_course_aaplication_form',
+											security: $response.attr('data-security'),
+											isocharset:isocharset,
+											label:JSON.stringify(labels),
+											data:JSON.stringify(formdata),
+										},
+									cache: false,
+									success: function (html) {
+										//
+									}
+							});
+						}, 2000);
+					}
+				});
+			});
+		</script>
+		<?php
 	}
 	
 } // END class WPLMS_Application_Forms_Init
